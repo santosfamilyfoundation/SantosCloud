5cfadc618aa45d4ee51f9653f71da235b7c0b60b
Jacob Riedel <jr112394@gmail.com>
2016-11-04 17:35:13 -0400

    ADD: api.py barebones for API calls

---
+++ api.py
@@ -0,0 +1,28 @@
+
+###This will hold all api calls for TrafficInTheCloud
+
+def uploadFiles(types, paths, *args):
+    pass
+
+def runConfigTest(uuid, config, frames, ret_type, ret_args, *args):
+    pass
+
+def runTrajectoryAnalysis(uuid, config, ret_type, ret_args, *args):
+    pass
+
+def runSafetyAnalysis(uuid, prediction, db, ret_type, ret_args):
+    pass
+
+def runVisualization(uuid, db, ret_form, ret_type, ret_args, *args):
+    pass
+
+def getDB(uuid):
+    pass
+
+def getStatus(uuid):
+    pass
+
+def generateDefaultConfig():
+    pass
+
+
