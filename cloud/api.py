77dba5c7e18e1d2367640c2bd1b27fcbbd7f97d6
Jacob Riedel <jr112394@gmail.com>
2016-11-04 20:27:43 -0400

    ADD: Video creation separated out to video.py to reduce bulk

--- cloud/api.py
+++ cloud/api.py
@@ -6,6 +6,7 @@ import shutil
 from app_config import AppConfig as ac
 import pm
 import subprocess
+import video
 
 def saveFiles(types, uuid, *args):
     ac.load_application_config()
@@ -17,53 +18,55 @@ def runConfigTest(uuid, config, frames, ret_type, ret_args, *args):
 
 def runTrajectoryAnalysis(uuid):#, config, ret_type, ret_args, *args):
     
-    def run(self):
-        """
-        Runs TrafficIntelligence trackers and support scripts.
-        """
-        pm.load_project(ac.CURRENT_PROJECT_PATH)
+def run(self):
+    """
+    Runs TrafficIntelligence trackers and support scripts.
+    """
+    pm.load_project(ac.CURRENT_PROJECT_PATH)
 
-        # create test folder
-        if not os.path.exists(ac.CURRENT_PROJECT_PATH + "/run"):
-            os.mkdir(ac.CURRENT_PROJECT_PATH + "/run")
+    # create test folder
+    if not os.path.exists(ac.CURRENT_PROJECT_PATH + "/run"):
+        os.mkdir(ac.CURRENT_PROJECT_PATH + "/run")
 
-        # removes object tracking.cfg
-        if os.path.exists(ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg"):
-            os.remove(ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg")
+    # removes object tracking.cfg
+    if os.path.exists(ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg"):
+        os.remove(ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg")
 
-        # creates new config file
-        shutil.copyfile(ac.CURRENT_PROJECT_PATH + "/.temp/test/test_object/object_tracking.cfg", ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg")
+    # creates new config file
+    shutil.copyfile(ac.CURRENT_PROJECT_PATH + "/.temp/test/test_object/object_tracking.cfg", ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg")
 
-        path1 = ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg"
+    path1 = ac.CURRENT_PROJECT_PATH + "/run/run_tracking.cfg"
 
-        f = open(path1, 'r')
-        lines = f.readlines()
-        f.close()
-        with open(path1, "w") as wf:
-            for line in lines:
-                line_param = line.split('=')[0].strip()
-                if "frame1" == line_param:  # Replace parameter "frame1"
-                    wf.write("frame1 = 0\n")
-                elif "nframes" == line_param:  # Remove parameter "nframes"
-                    wf.write("nframes = 0\n")
-                elif "database-filename" == line_param:
-                    wf.write("database-filename = results.sqlite\n")
-                else:
-                    wf.write(line)
+    f = open(path1, 'r')
+    lines = f.readlines()
+    f.close()
+    with open(path1, "w") as wf:
+        for line in lines:
+            line_param = line.split('=')[0].strip()
+            if "frame1" == line_param:  # Replace parameter "frame1"
+                wf.write("frame1 = 0\n")
+            elif "nframes" == line_param:  # Remove parameter "nframes"
+                wf.write("nframes = 0\n")
+            elif "database-filename" == line_param:
+                wf.write("database-filename = results.sqlite\n")
+            else:
+                wf.write(line)
 
-        db_path = os.path.join(ac.CURRENT_PROJECT_PATH, "run", "results.sqlite")
-        tracking_path = os.path.join(ac.CURRENT_PROJECT_PATH, "run", "run_tracking.cfg")
+    db_path = os.path.join(ac.CURRENT_PROJECT_PATH, "run", "results.sqlite")
+    tracking_path = os.path.join(ac.CURRENT_PROJECT_PATH, "run", "run_tracking.cfg")
 
-        if os.path.exists(db_path):  # If results database already exists,
-            os.remove(db_path)  # then remove it--it'll be recreated.
-        subprocess.call(["feature-based-tracking", tracking_path, "--tf", "--database-filename", db_path])
-        subprocess.call(["feature-based-tracking", tracking_path, "--gf", "--database-filename", db_path])
+    if os.path.exists(db_path):  # If results database already exists,
+        os.remove(db_path)  # then remove it--it'll be recreated.
+    subprocess.call(["feature-based-tracking", tracking_path, "--tf", "--database-filename", db_path])
+    subprocess.call(["feature-based-tracking", tracking_path, "--gf", "--database-filename", db_path])
 
-        subprocess.call(["classify-objects.py", "--cfg", tracking_path, "-d", db_path])  # Classify road users
+    subprocess.call(["classify-objects.py", "--cfg", tracking_path, "-d", db_path])  # Classify road users
 
-        db_make_objtraj(db_path)  # Make our object_trajectories db table
+    db_make_objtraj(db_path)  # Make our object_trajectories db table
 
-        self.create_video()
+    video.create_video()
+
+
 
 def runSafetyAnalysis(uuid, prediction, db, ret_type, ret_args):
     pass
