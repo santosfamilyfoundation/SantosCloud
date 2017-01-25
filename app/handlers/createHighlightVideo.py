156f383ce039d8cc2273f35e0982dc0b845a1046
youralien <ryan.louie@students.olin.edu>
2017-01-25 16:06:25 -0500

    UPDATE: CreateHighlightVideoHandler works

--- app/handlers/createHighlightVideo.py
+++ app/handlers/createHighlightVideo.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
 
+import os
 import tornado.web
+import tornado.escape
+from trafficcloud.video import create_highlight_video, get_framerate
+from storage import alterInteractionsWithRoadUserType, getNearMissFrames
+from trafficcloud.app_config import get_project_path, get_project_video_path
 
-class CreateHighlightVideoHandler(tornado.web.RequestHandler):
+# TODO: remove once pip's error request handler is written
+import json
+import traceback
+class MyAppBaseHandler(tornado.web.RequestHandler):
+
+    def write_error(self, status_code, **kwargs):
+
+        self.set_header('Content-Type', 'application/json')
+        if self.settings.get("serve_traceback") and "exc_info" in kwargs:
+            # in debug mode, try to send a traceback
+            lines = []
+            for line in traceback.format_exception(*kwargs["exc_info"]):
+                lines.append(line)
+            self.finish(json.dumps({
+                'error': {
+                    'code': status_code,
+                    'message': self._reason,
+                    'traceback': lines,
+                }
+            }))
+        else:
+            self.finish(json.dumps({
+                'error': {
+                    'code': status_code,
+                    'message': self._reason,
+                }
+}))
+
+class CreateHighlightVideoHandler(MyAppBaseHandler):
     """
     @api {post} /highlightVideo/ Highlight Video
     @apiName HighlightVideo
@@ -11,12 +44,57 @@ class CreateHighlightVideoHandler(tornado.web.RequestHandler):
     @apiDescription Calling this route will create a highlight video of dangerous interactions from a specified project. When the video is created, an email will be sent to the project's user. This route requires running object tracking on the video, and then running safety analysis on the results of the object tracking beforehand. (Due to the potentially long duration, it is infeasible to return the results as a response to the HTTP request. In order to check the status of the testing and view results, see the Status group of messages.)
 
     @apiParam {String} identifier The identifier of the project to create a highlight video for.
-    @apiParam {Integer} [ttc_threshold] Threshold for determining whether an interaction is dangerous.
-    @apiParam {Integer} [vehicle_only] Flag for specifying only vehicle-vehicle interactions
+    @apiParam {Float} [ttc_threshold] Threshold for determining whether an interaction is dangerous. Default 1.5 seconds.
+    @apiParam {Integer} [vehicle_only] Flag for specifying only vehicle-vehicle interactions. Default to True.
 
     @apiSuccess status_code The API will return a status code of 200 upon success.
 
     @apiError error_message The error message to display.
     """
     def post(self):
-        self.finish("Create Highlight Video")
+        identifier = self.get_body_argument('identifier')
+        ttc_threshold = float(self.get_body_argument('ttc_threshold', default=1.5))
+        vehicle_only = bool(self.get_body_argument('vehicle_only', default=True))
+
+        status_code, reason = CreateHighlightVideoHandler.handler(identifier, ttc_threshold, vehicle_only)
+        if status_code == 200:
+            self.finish("Create Highlight Video")
+        else:
+            raise tornado.web.HTTPError(reason=reason, status_code=status_code)
+
+    @staticmethod
+    def handler(identifier, ttc_threshold, vehicle_only):
+        project_dir = get_project_path(identifier)
+        if not os.path.exists(project_dir):
+            return (500, 'Project directory does not exist. Check your identifier?')
+
+        db = os.path.join(project_dir, 'run', 'results.sqlite')
+        if not os.path.exists(db):
+            return (500, 'Database file does not exist. Trajectory analysis needs to be called first ')
+
+        video_path = get_project_video_path(identifier)
+        if not os.path.exists(video_path):
+            return (500, 'Source video file does not exist.  Was the video uploaded?')
+
+        try:
+            alterInteractionsWithRoadUserType(db)
+        except Exception as error_message:
+            return (500, "Alter Interactions Table with Road User Type failed\n" + error_message)
+
+        ttc_threshold_frames = int(ttc_threshold * float(get_framerate(video_path)))
+
+        try:
+            near_misses = getNearMissFrames(db, ttc_threshold_frames, vehicle_only)
+        except Exception as error_message:
+            return (500, error_message)
+
+        try:
+            create_highlight_video(project_dir, video_path, near_misses)
+        except Exception as error_message:
+            return (500, error_message)
+
+
+        return (200, "Success")
+
+
+
