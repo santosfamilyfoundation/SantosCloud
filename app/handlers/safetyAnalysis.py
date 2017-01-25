0cde35c5e8b7d5388aa93b78f1e3a2b98771e02c
Deniz Celik <deniz.celik@students.olin.edu>
2017-01-25 01:23:55 -0500

    ADD: email param to analysis group functions

--- app/handlers/safetyAnalysis.py
+++ app/handlers/safetyAnalysis.py
@@ -11,6 +11,7 @@ class SafetyAnalysisHandler(tornado.web.RequestHandler):
     @apiDescription Calling this route will perform safety analysis on a project that object tracking has already been run on. When the analysis is done, an email will be sent to the project's user. (Due to the potentially long run duration, it is infeasible to return the results as a response to the HTTP request. In order to check the status of the testing and view results, see the Status group of messages.)
 
     @apiParam {String} identifier The identifier of the project on which to run safety analysis.
+    @apiParam {String} email The email address that should be notified when the safety analysis is complete
 
     @apiSuccess status_code The API will return a status code of 200 upon success.
 
