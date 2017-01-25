36899d04e08a1fb447aeb0791c00b0d2bdd2de7c
Deniz Celik <deniz.celik@students.olin.edu>
2017-01-25 01:18:06 -0500

    MOD: Fixing groupings

--- app/handlers/makeReport.py
+++ app/handlers/makeReport.py
@@ -7,7 +7,7 @@ class MakeReportHandler(tornado.web.RequestHandler):
     @api {post} /makeReport/ Make Report
     @apiName MakeReport
     @apiVersion 0.0.0
-    @apiGroup Analysis
+    @apiGroup Results
     @apiDescription Calling this route will create a safety report for a specified project. When the report is created, an email will be sent to the project's user. This route requires running object tracking on the video, and then running safety analysis on the results of the object tracking beforehand. (Due to the potentially long duration, it is infeasible to return the results as a response to the HTTP request. In order to check the status of the testing and view results, see the Status group of messages.)
 
     @apiParam {String} identifier The identifier of the project for which to create the report.
--- app/handlers/roadUserCounts.py
+++ app/handlers/roadUserCounts.py
@@ -7,7 +7,7 @@ class RoadUserCountsHandler(tornado.web.RequestHandler):
     @api {post} /roadUserCounts/ Road User Counts
     @apiName RoadUserCounts
     @apiVersion 0.0.0
-    @apiGroup Analysis
+    @apiGroup Results
     @apiDescription Calling this route will create a road user counts image from a specified project. When the image is created, an email will be sent to the project's user. This route requires running object tracking on the video, and then running safety analysis on the results of the object tracking beforehand. (Due to the potentially long duration, it is infeasible to return the results as a response to the HTTP request. In order to check the status of the testing and view results, see the Status group of messages.)
 
     @apiParam {String} identifier The identifier of the project to create road user counts for.
