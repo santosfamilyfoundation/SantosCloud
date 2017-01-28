bab6011b898ce5e31d3d7574b287f26b391698f1
Sawyer Vaughan <sawyerv726@gmail.com>
2017-01-28 09:23:42 -0800

    Refactor pm.py and add config route

--- trafficcloud/pm.py
+++ trafficcloud/pm.py
@@ -17,84 +17,102 @@ import numpy as np
 from app_config import get_project_path, get_project_config_path, config_section_exists, update_config_without_sections
 from video import get_framerate
 
-import subprocess
-import uuid
+def create_project(identifier, video_filename):
+    config_dict = {}
+    _update_config_dict_with_defaults(config_dict)
 
-class ProjectWizard():
+    _create_project_dir(identifier, config_dict)
 
-    def __init__(self,files):
-        self.dict_files = files
+def update_homography(identifier, homography_path, unitpixelratio):
+    pass
 
-        self.identifier = str(uuid.uuid4())
-        self.create_project_dir(self.identifier)
+def update_project_config(identifier, config_dict):
+    _update_config_dict_with_defaults(config_dict)
 
-    def create_project_dir(self, identifier):
-        directory_names = ["homography", os.path.join(".temp", "test", "test_object"), os.path.join(".temp", "test", "test_feature"), "run", "results"]
-        project_path = get_project_path(identifier)
+    project_path = get_project_path(identifier)
+    tracking_path = os.path.join(project_path, "tracking.cfg")
+    update_config_without_sections(tracking_path, config_dict)
 
-        if not os.path.exists(project_path):
-            # Create proj dirs
-            for new_dir in directory_names:
-                os.makedirs(os.path.join(project_path, new_dir))
+def default_config_dict():
+    return {
+        'max_features_per_frame': 1000,
+        'num_displacement_frames': 10,
+        'min_feature_displacement': 0.0001,
+        'max_iterations_to_persist': 200,
+        'min_feature_frames': 15,
+        'max_connection_distance': 1.0,
+        'max_segmentation_distance': 0.7,
+    }
 
-            # Write files from client
-            for key,value in self.dict_files.iteritems():
-                if key[:5] == 'video':
-                    self.videopath = os.path.join(project_path, key)
-                with open(os.path.join(project_path, key), 'wb') as fh:
-                    fh.write(value)
+def _update_config_dict_with_defaults(config_dict):
+    default_dict = default_config_dict()
 
+    # Remove things that we don't want the user to be able to configure
+    for key in config_dict.keys():
+        if key not in default_dict.keys():
+            del config_dict[key]
 
-            self._write_to_project_config(identifier)
+    # Add defaults for things that don't exist
+    for (key, value) in default_dict.iteritems():
+        if key not in config_dict.keys():
+            config_dict[key] = value
 
-            default_files_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "default")
+def _create_project_dir(identifier, config_dict, video_filename):
+    test_object_dir = os.path.join(".temp", "test", "test_object")
+    test_feature_dir = os.path.join(".temp", "test", "test_feature")
 
-            copy(os.path.join(default_files_dir, "tracking.cfg"), os.path.join(project_path, "tracking.cfg"))
-            copy(os.path.join(default_files_dir, "classifier.cfg"), os.path.join(project_path, "classifier.cfg"))
+    directory_names = ["homography", test_object_dir, test_feature_dir, "run", "results"]
+    project_path = get_project_path(identifier)
 
-            update_dict_1 = {
-                'classifier-filename': os.path.join(project_path, 'classifier.cfg')
-            }
-            update_config_without_sections(os.path.join(project_path, 'tracking.cfg'), update_dict_1)
+    if not os.path.exists(project_path):
+        # Create proj dirs
+        for new_dir in directory_names:
+            os.makedirs(os.path.join(project_path, new_dir))
 
-            update_dict_2 = {
-                'pbv-svm-filename': os.path.join(default_files_dir, 'modelPBV.xml'),
-                'bv-svm-filename': os.path.join(default_files_dir, 'modelBV.xml')
-            }
-            update_config_without_sections(os.path.join(project_path, 'classifier.cfg'), update_dict_2)
+        # TODO: unitpixelratio
+        _write_to_project_config(identifier)
 
-        else:
-            print("Project exists. No new project created.")
+        default_files_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "default")
 
-    def _write_to_project_config(self, identifier):
-        # Copy information given by the project_name.cfg generated client side
-        client_config_parser = SafeConfigParser()
-        client_config_parser.read(os.path.join(get_project_path(identifier), "project_name.cfg"))
-        unitpixelratio = None
-        try:
-            unitpixelratio = client_config_parser.get("homography", "unitpixelratio")
-        except NoSectionError:
-            print("NoSectionError: no 'homography' section found in config file. Unit Pixel Ratio will not be copied.")
+        tracking_path = os.path.join(project_path, "tracking.cfg")
+        classifier_path = os.path.join(project_path, "classifier.cfg")
 
-        ts = time.time()
-        vid_ts = datetime.datetime.now()
-        #This line needs to be updated to no longer need the ui class. Load video and pull time.
-        timestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S %Z')
-        video_timestamp = vid_ts.strftime('%d-%m-%Y %H:%M:%S %Z')
-        config_parser = SafeConfigParser()
-        config_parser.add_section("info")
-        config_parser.set("info", "project_name", identifier)
-        config_parser.set("info", "creation_date", timestamp)
-        config_parser.add_section("video")
-        config_parser.set("video", "name", os.path.basename(self.videopath))
-        config_parser.set("video", "source", self.videopath)
-        config_parser.set("video", "framerate", get_framerate(self.videopath))
-        config_parser.set("video", "start", video_timestamp)
-        if unitpixelratio:
-            config_parser.add_section("homography")
-            config_parser.set("homography", "unitpixelratio", unitpixelratio)
+        copy(os.path.join(default_files_dir, "tracking.cfg"), tracking_path)
+        copy(os.path.join(default_files_dir, "classifier.cfg"), classifier_path)
 
-        with open(get_project_config_path(identifier), 'wb') as configfile:
-            config_parser.write(configfile)
+        # Add classifier path along with configuration
+        config_dict['classifier-filename'] = classifier_path
+        update_config_without_sections(tracking_path, config_dict)
 
-        os.remove(os.path.join(get_project_path(identifier), "project_name.cfg"))
+        update_dict = {
+            'pbv-svm-filename': os.path.join(default_files_dir, 'modelPBV.xml'),
+            'bv-svm-filename': os.path.join(default_files_dir, 'modelBV.xml')
+        }
+        update_config_without_sections(classifier_path, update_dict)
+
+    else:
+        print("Project exists. No new project created.")
+
+def _write_to_project_config(self, identifier, video_filename, unitpixelratio=None):
+    ts = time.time()
+    vid_ts = datetime.datetime.now()
+    #This line needs to be updated to no longer need the ui class. Load video and pull time.
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S %Z')
+    video_timestamp = vid_ts.strftime('%d-%m-%Y %H:%M:%S %Z')
+    config_parser = SafeConfigParser()
+    config_parser.add_section("info")
+    config_parser.set("info", "project_name", identifier)
+    config_parser.set("info", "creation_date", timestamp)
+    config_parser.add_section("video")
+    config_parser.set("video", "name", os.path.basename(video_filename))
+    config_parser.set("video", "source", video_filename)
+    config_parser.set("video", "framerate", get_framerate(video_filename))
+    config_parser.set("video", "start", video_timestamp)
+    if unitpixelratio:
+        config_parser.add_section("homography")
+        config_parser.set("homography", "unitpixelratio", unitpixelratio)
+
+    with open(get_project_config_path(identifier), 'wb') as configfile:
+        config_parser.write(configfile)
+
+    os.remove(os.path.join(get_project_path(identifier), "project_name.cfg"))
