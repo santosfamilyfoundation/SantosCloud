9880081c3cdcb5ee04f4b70cc5096b37c9d010f7
Deniz Celik <deniz.celik@students.olin.edu>
2017-01-29 03:11:54 +0000

    MOD: uploadHomography now computes homography

--- app/handlers/uploadHomography.py
+++ app/handlers/uploadHomography.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import tornado.web
+from tornado.escape import json_decode
 import os
-from trafficcloud.app_config import get_project_path
+from traffic_cloud_utils.app_config import get_project_path
+import numpy as np
+import cv2
 
 class UploadHomographyHandler(tornado.web.RequestHandler):
     """
@@ -28,11 +31,36 @@ class UploadHomographyHandler(tornado.web.RequestHandler):
     def post(self):
         self.files = self.request.files
         self.identifier = self.get_body_argument('identifier')
+        self.up_ratio = float(self.get_body_argument('unit_pixel_ratio'))
         self.write_homography_files()
         self.finish("Upload Homography")
         
     def write_homography_files(self):
         project_dir = get_project_path(self.identifier)
+        num_points = int(self.get_body_argument('num_points')) 
+        points = [ (self.get_body_arguments('p'+str(i))[0],\
+                    self.get_body_arguments('p'+str(i))[1]) for i in xrange(num_points)]
+
+        homography = self.handler(self.up_ratio, points)
+        np.savetxt(os.path.join(project_dir,'homography','homography.txt'),homography)
+
         for key,value in self.files.iteritems():
             with open(os.path.join(project_dir,'homography',value[0]['filename']), 'wb') as f:
                 f.write(value[0]['body'])
+
+    @staticmethod
+    def handler(up_ratio, pts):
+        aerial, camera=  zip(*pts)
+
+        unscaled_world = []
+        for i in aerial:
+            h,t = i.split(', ')
+            unscaled_world.append((float(h[1:]),float(t[:-2])))
+
+        scaled_video = []
+        for i in camera:
+            h,t = i.split(', ')
+            scaled_video.append((float(h[1:]),float(t[:-2])))
+
+        homography, mask = cv2.findHomography(up_ratio*np.array(unscaled_world), np.array(scaled_video))
+        return homography
