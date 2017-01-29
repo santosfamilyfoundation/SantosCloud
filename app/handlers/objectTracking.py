aa878286e9ddb69ed0495429aa59396fce6baa76
Jacob Riedel <jr112394@gmail.com>
2017-01-28 19:21:49 -0500

    Corrected import

--- app/handlers/objectTracking.py
+++ app/handlers/objectTracking.py
@@ -5,8 +5,7 @@ import shutil
 
 import tornado.web
 
-from plotting.make_object_trajectories import main as db_make_objtraj
-
+from traffic_cloud_utils.make_object_trajectories import main as db_make_objtraj
 from traffic_cloud_utils.app_config import get_project_path, get_project_video_path, update_config_without_sections, get_config_without_sections
 from traffic_cloud_utils.emailHelper import EmailHelper
 import trafficcloud.video
