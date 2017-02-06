94ebf7451926cfaa4895c623af8e60d155aefc23
Ryan Louie <ryan.louie@students.olin.edu>
2017-02-06 18:42:25 -0500

    ADD: make pdf report request handler (#45)
    * ADD: make pdf report request handler
    
    Summary:
    
    - request handler takes all viz in final_images and puts them into a report called santosreport.pdf
    - changed makePdf to take a out_path, so that the report could be saved in a different directory from final_images
    - report saved as landscape letter size, with the assumption that the visualization images are landscape as well
    
    * Added newline at EOF of makeReport.py

--- app/traffic_cloud_utils/pdf_generate.py
+++ app/traffic_cloud_utils/pdf_generate.py
@@ -2,23 +2,32 @@ import os
 from fpdf import FPDF
 from PIL import Image
 
-def makePdf(pdfFileName, listImages, dir = ''):
+def makePdf(out_path, listImages, dir = ''):
     """
     Arguments
     ---------
-    pdfFileName: str, i.e. 'output'
+    out_path: str, i.e. 'path/to/output.pdf'
     listImages: list of str, i.e. ['road_user_type_counts.png', 'speed_cdf.png']
     dir: str, path where images located, and pdf will be outputted
     """
-    cover = Image.open(os.path.join(dir, listImages[0]))
-    width, height = cover.size
 
-    pdf = FPDF(unit = "pt", format = [width, height])
+    pdf = FPDF(orientation='L', unit = "in", format = 'Letter')
 
     # Save an image per page
     for page in listImages:
         pdf.add_page()
-        pdf.image(os.path.join(dir,str(page)), 0, 0)
 
-    pdf.output(os.path.join(dir, pdfFileName, ".pdf"), "F")
+        img_path = os.path.join(dir,str(page))
+        img = Image.open(img_path)
+        w, h = img.size
+        
+        # all images default 8 inch width
+        width = 8
+
+        # height in inches, relative to aspect ratio
+        height = width * (h // w)
+
+        pdf.image(img_path, x=1, y=1, w=width, h=height)
+
+    pdf.output(out_path, "F")
 
