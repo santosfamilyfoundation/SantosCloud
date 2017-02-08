5da8d3c461cb3e358d30718bc584ee85549c0de0
Philip Seger <seger.philip@gmail.com>
2017-02-08 15:05:12 -0500

    Retrieve results (#48)
    * ADD: Begin zip process for file writeback
    
    * MOD: stream the highlight video file
    
    * EDIT: zip file and download untested
    
    * EDIT: syntax
    
    * EDIT: get body args method naming
    
    * EDIT: I don't like atom
    
    * EDIT: writing the right file now
    
    * EDIT: fix for now, need to get zip working
    
    * EDIT: file path naming
    
    * EDIT: file path args
    
    * EDIT: cleaned up for PR
    
    * EDIT: better zip filepath, pr comments
    
    * EDIT: error str and one letter identifier fix
    
    * EDIT: file name and added the report result
    
    * EDIT: remove zip after sending

--- app/handlers/retrieveResults.py
+++ app/handlers/retrieveResults.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-
+import zipfile
+import os
 import tornado.web
 from baseHandler import BaseHandler
+
+from traffic_cloud_utils.app_config import get_project_path, get_project_video_path, update_config_without_sections, get_config_without_sections
+from traffic_cloud_utils import video
+from traffic_cloud_utils.emailHelper import EmailHelper
 
 class RetrieveResultsHandler(BaseHandler):
     """
@@ -18,4 +23,43 @@ class RetrieveResultsHandler(BaseHandler):
     @apiError error_message The error message to display.
     """
     def get(self):
-        self.finish("Retrieve Results")
+        chunk_size = 2048
+        identifier = self.get_body_argument('identifier')
+        project_path = get_project_path(identifier)
+        file_videos = os.path.join(project_path, 'final_videos')
+        file_images = os.path.join(project_path, 'final_images')
+        file_report = os.path.join(project_path, 'santosreport.pdf')
+        self.file_name = os.path.join(project_path, 'results.zip')
+
+        zipf = zipfile.ZipFile(self.file_name, 'w', zipfile.ZIP_DEFLATED)
+        # Write videos
+        for root, dirs, files in os.walk(file_videos):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, file)
+        # Write images
+        for root, dirs, files in os.walk(file_images):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, file)
+        # Write report
+        zipf.write(file_report, os.path.basename(file_report))
+        zipf.close()
+
+        self.set_header('Content-Type', 'application/octet-stream')
+        self.set_header('Content-Description', 'File Transfer')
+        self.set_header('Content-Disposition', 'attachment; filename=' + self.file_name)
+        with open(self.file_name, 'rb') as f:
+            try:
+                while True:
+                    data = f.read(chunk_size)
+                    if not data:
+                        break
+                    self.write(data)
+                self.finish()
+            except Exception as e:
+                self.error_message = str(e)
+                raise tornado.web.HTTPError(status_code=500)
+
+    def on_finish(self):
+        os.remove(self.file_name)
