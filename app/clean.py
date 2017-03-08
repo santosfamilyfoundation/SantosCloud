ca52583e2b1650256f149b27f07255cd5614731f
youralien <ryan.louie@students.olin.edu>
2017-03-08 16:59:44 +0000

    Add delete_object and change_user_type functions

--- app/clean.py
+++ app/clean.py
@@ -1,3 +1,10 @@
+import sqlite3
+
+# user to index mapping in the database
+user_abbr_map = {'c': 1, 'p': 2, 'b': 4}
+userlist = ['unknown', 'car', 'pedestrian',
+            'motorcycle', 'bicycle', 'bus', 'truck']
+
 
 ## Clean step
 def clean(db):
@@ -61,4 +68,71 @@ def execute_command(db):
         change_user_types(db, objs_to_change, t)
 
     return True
+
+def change_user_types(db, object_id, type_as_abbr):
+    """
+    object_id: int, i.e. 14
+    type_as_abbr: str, i.e. c, p, b for car, ped, or bike
+    """
+    
+    object_id = int(object_id)
+    type_as_abbr = type_as_abbr.lower()
+    
+    conn = sqlite3.connect('results.sqlite')
+    cur = conn.cursor()
+
+    queryStatement = 'UPDATE objects SET road_user_type = ? WHERE object_id = ?'
+    cur.execute(queryStatement, (user_abbr_map[type_as_abbr], object_id))
+
+    conn.commit()
+    conn.close()
+
+def delete_object(db, object_id):
+    """ Removes object and its associate trajectory from the database.
+    object_id: int, i.e. 14
+    """
+    object_id = int(object_id)
+
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+ 
+    #
+    # Delete from objects table
+    #
+    queryStatement = 'DELETE FROM objects WHERE object_id = ?' 
+    print queryStatement
+    cur.execute(queryStatement, (object_id,))
+
+    #
+    # Extract matching trajectory_ids
+    #
+    queryStatement = 'SELECT * FROM objects_features WHERE object_id = ?' 
+    print queryStatement
+    cur.execute(queryStatement, (object_id,))
+
+    trajectory_ids_list = []
+    for row in cur:
+        object_id1, trajectory_id = row
+        trajectory_ids_list.append(trajectory_id)
+
+    print trajectory_ids_list
+
+    for trajectory_id in trajectory_ids_list:
+        print "trajectory_id: ", trajectory_id
+        #
+        # Delete from objects_features
+        #
+        queryStatement = 'DELETE FROM objects_features WHERE object_id = ?' 
+        cur.execute(queryStatement, (object_id,))
+
+        #
+        # Delete from positions and velocities for the object's trajectory_id
+        #
+        queryStatement = 'DELETE FROM positions WHERE trajectory_id = ?'
+        cur.execute(queryStatement, (trajectory_id,))
+        queryStatement = 'DELETE FROM velocities WHERE trajectory_id = ?'
+        cur.execute(queryStatement, (trajectory_id,))
+        
+    # Commit once everything has been deleted
+    conn.commit()
 
